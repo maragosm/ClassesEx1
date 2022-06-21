5fcb984b1c0fca7ab0852a4c0b79f26faa4eb789
Michael Maragos <maragosm@aol.com>
2022-06-21 14:25:39 -0400

    initial commit

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_ex1
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void CarShow(string carMake, string carModel, int year)
+        {
+            Console.WriteLine($"The best car in the world is the {year} {carMake} {carModel}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Classes_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PTCruiser = new Car();
+                PTCruiser.Make = "Chrysler";
+                PTCruiser.Model = "PT Cruiser";
+                PTCruiser.Year = 2001;
+
+            PTCruiser.CarShow(PTCruiser.Make, PTCruiser.Model, PTCruiser.Year);
+        }
+    }
+}
